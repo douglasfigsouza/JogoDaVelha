5b8faa7d46998783036642eb37bb94231e1b8b21
Douglas <douglasfigsouza@gmail.com>
2017-08-31 10:27:28 -0300

    Estrutura Criada

--- JogoDaVelha/App_Start/BundleConfig.cs
+++ JogoDaVelha/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace JogoDaVelha
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/Custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
--- JogoDaVelha/App_Start/RouteConfig.cs
+++ JogoDaVelha/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace JogoDaVelha
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Jogo", action = "IniciaJogo", id = UrlParameter.Optional }
             );
         }
     }
