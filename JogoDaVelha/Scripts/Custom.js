f2148c73df98a44a0c4753106012f001c12e0fef
Douglas <douglasfigsouza@gmail.com>
2017-08-31 15:50:15 -0300

    Criação da function Cadastro

--- JogoDaVelha/Scripts/Custom.js
+++ JogoDaVelha/Scripts/Custom.js
@@ -1,4 +1,6 @@
-﻿var Jogador1;
+﻿var flag;
+var pecaJog1; 
+var pecaJog2;
 // função que escolhe x ou bolinha
 function SelecionaPeca(jog, op) {
     switch (jog)
@@ -6,24 +8,29 @@ function SelecionaPeca(jog, op) {
         case "Jog1":
             if (op == 'X')
             {
-                document.getElementById("opJog1O").disabled = true;
+                pecaJog1 = "X";
+                document.getElementById("opJog2X").disabled = true;
                 document.getElementById("opJog2O").removeAttribute('disabled');
 
             }
             else
             {
-                document.getElementById("opJog1X").disabled = true;
+                pecaJog1 = "O";
+                document.getElementById("opJog2O").disabled = true;
                 document.getElementById("opJog2X").removeAttribute('disabled');
             }
             break;
         case "Jog2":
-            if (op == 'X') {
-                document.getElementById("opJog2O").disabled = true;
+            if (op == 'X')
+            {
+                pecaJog2 = "X";
+                document.getElementById("opJog1X").disabled = true;
                 document.getElementById("opJog1O").removeAttribute('disabled');
             }
             else
             {
-                document.getElementById("opJog2X").disabled = true;
+                pecaJog2="O"
+                document.getElementById("opJog1O").disabled = true;
                 document.getElementById("opJog1X").removeAttribute('disabled');
             }
             break;
@@ -31,11 +38,33 @@ function SelecionaPeca(jog, op) {
 }
 function CadastraJogadores()
 {
-    var form1 = document.getElementById('formCadJog');
-    Jogador1 = form1.Jog1.value;
-    alert(Jogador1);
+    var form = document.getElementById('formCadJog');
+    var NomeJog1 = form.Jog1.value;
+    var NomeJog2 = form.Jog2.value;
+    var inicio = form.inicia.value;
+    alert(inicio);
+    
+    if (inicio == 1) {
+        pecaJog1  = form.opJog1.value;
+        document.getElementById("jogadorInicial").innerText = "Jogador Inicial:" + NomeJog1;
+        document.getElementById("peca").innerText = "Simbolo:" + peca;
+        flag = 1;
+    }
+    else
+    {
+        pecaJog2 = form.opJog2.value;
+        document.getElementById("jogadorInicial").innerText = "Jogador Inicial:" + NomeJog2;
+        document.getElementById("peca").innerText = "Jogador Inicial:" + peca;
+        flag = 2;
+    }
+    $("#Jogadores").addClass('DisplayNone');
+    $("#jogo").removeClass("DisplayNone");
+    $("#jogo").addClass("DisplayBlock");
 }
 function Jogada(op)
 {
-    document.getElementById(op).innerText = "X";
+    if (flag == 1)
+    {
+        document.getElementById(op).innerText = "X";
+    }
 }
